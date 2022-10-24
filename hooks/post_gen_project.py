859698f0830eff5de871f304e844ba1a09c3777f
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2022-10-24 14:27:22 +0100

    Make initial commit after gen

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,3 +1,12 @@
 import subprocess as sp
 
 sp.run(["git", "init"], check=True)
+sp.run(["git", "add", "."], check=True)
+
+msg = """
+Initial commit
+
+Created repository from template.
+""".lstrip()
+
+sp.run(["git", "commit", "-m", msg])
